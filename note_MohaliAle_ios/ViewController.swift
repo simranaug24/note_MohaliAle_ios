0f3701a625a8cfd381018de2d73b0bf2e1e01730
simranaug24 <simranaug24@gmail.com>
2021-02-02 15:45:56 +0530

    Initial Commit

---
+++ note_MohaliAle_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  note_MohaliAle_ios
+//
+//  Created by Amarvir Mac on 02/02/21.
+//  Copyright © 2021 Amarvir Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
